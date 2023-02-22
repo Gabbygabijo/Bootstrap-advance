61597c2b247bc8c6d48db24bd51cabf2949de8c5
Gabbygabijo <gabrielfashedemi@gmail.com>
2023-02-22 09:18:55 +0100

    something out of the box

--- asset/js/pops.js
+++ asset/js/pops.js
@@ -1,2 +1,14 @@
 // let name=prompt("What is your name?");
 // alert("Hello " + name + " !");
+const names = ["nine ", "today", "tomorrow", 33, 55, ];
+console.log(names[0] + names[3]);
+
+// function optIn(){
+//     document.getElementById("right").style.width="100%";
+//     document.getElementById("left").style.width="0";
+// };
+
+// function optOut(){
+//     document.getElementById("right").style.width="0";
+//     document.getElementById("left").style.width="100%";
+// };
